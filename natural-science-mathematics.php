0b5aab87005bfdccd4327ae3c1bfebaba6a46282
dhysa <deandhysa@gmail.com>
2015-08-25 11:53:09 +0700

    iseng

--- natural-science-mathematics.php
+++ natural-science-mathematics.php
@@ -1,104 +1,141 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
+<head>
 
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>nogoblog.com</title>
-        
-        <!-- Bootstrap Core CSS -->
-        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <title>nogoblog.com</title>
 
-        <!-- Custom CSS -->
-        <link href="css/style.css" rel="stylesheet">
-        
-        <!-- Template js -->
-        <script src="js/jquery-2.1.1.min.js"></script>
-        <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- Bootstrap Core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-         <!--[if lt IE 9]>
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Template js -->
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+    <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
 
-    </head>
-    
-    <body>
+</head>
 
-    	 <!-- Start Logo Section -->
-        <section id="logo-section" class="text-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="logo text-center">
-                            <img src="images/nogoblog.png">
-                            <h3>Mathematics - Natural Science</h3>
-                            <h4>Matematika - Ilmu Pengetahuan Alam</h4>
-                        </div>
+<body>
+
+    <!-- Start Logo Section -->
+    <section id="logo-section" class="text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="logo text-center">
+                        <img src="images/nogoblog.png">
+                        <h3>Mathematics - Natural Science</h3>
+                        <h4>Matematika - Ilmu Pengetahuan Alam</h4>
                     </div>
                 </div>
             </div>
-        </section>
-        <!-- End Logo Section -->
+        </div>
+    </section>
+    <!-- End Logo Section -->
 
-        <!-- Start Main Body Section -->
-        <div class="mainbody-section text-center">
-            <div class="container">
-                <div class="row">
-                	
-                  <!-- Start Panel Chapter -->
-                  <div class="panel-group" id="natural-science-math" role="tablist" aria-multiselectable="true">
+    <!-- Start Main Body Section -->
+    <div class="mainbody-section text-center">
+        <div class="container">
+            <div class="row">
+
+                <!-- Start Panel Chapter -->
+                <div class="panel-group" id="natural-science-math" role="tablist" aria-multiselectable="true">
                     <div class="panel panel-default">
-                      <div class="panel-heading" role="tab" id="gradeOne">
-                        <h4 class="panel-title">
+                        <div class="panel-heading" role="tab" id="gradeOne">
+                            <h4 class="panel-title">
                           <a class="collapsed" role="button" data-toggle="collapse" data-parent="#natural-science-math" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                             Grade 10
                           </a>
                         </h4>
-                      </div>
-                      <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="gradeOne">
-                        <div class="panel-body">
-                          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                         </div>
-                      </div>
+                        <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="gradeOne">
+                            <div class="panel-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="menu-item maroon">
+                                            <a href="#about-modal" data-toggle="modal">
+                                                <i class="fa fa-user"></i>
+                                                <p>Physics</p>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="menu-item maroon">
+                                            <a href="#about-modal" data-toggle="modal">
+                                                <i class="fa fa-user"></i>
+                                                <p>Math-Science</p>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="menu-item maroon">
+                                            <a href="#about-modal" data-toggle="modal">
+                                                <i class="fa fa-user"></i>
+                                                <p>Biology</p>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="menu-item maroon">
+                                            <a href="#about-modal" data-toggle="modal">
+                                                <i class="fa fa-user"></i>
+                                                <p>Chemistry</p>
+                                            </a>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+                        </div>
                     </div>
                     <div class="panel panel-default">
-                      <div class="panel-heading" role="tab" id="gradeTwo">
-                        <h4 class="panel-title">
+                        <div class="panel-heading" role="tab" id="gradeTwo">
+                            <h4 class="panel-title">
                           <a class="collapsed" role="button" data-toggle="collapse" data-parent="#natural-science-math" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                             Grade 11
                           </a>
                         </h4>
-                      </div>
-                      <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="gradeTwo">
-                        <div class="panel-body">
-                          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                         </div>
-                      </div>
+                        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="gradeTwo">
+                            <div class="panel-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
                     </div>
                     <div class="panel panel-default">
-                      <div class="panel-heading" role="tab" id="gradeThree">
-                        <h4 class="panel-title">
+                        <div class="panel-heading" role="tab" id="gradeThree">
+                            <h4 class="panel-title">
                           <a class="collapsed" role="button" data-toggle="collapse" data-parent="#natural-science-math" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                             Grade 12
                           </a>
                         </h4>
-                      </div>
-                      <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="gradeThree">
-                        <div class="panel-body">
-                          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                         </div>
-                      </div>
+                        <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="gradeThree">
+                            <div class="panel-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <!-- End Panel Chapter -->
-
                 </div>
+                <!-- End Panel Chapter -->
+
             </div>
         </div>
-    </body>
-    
+    </div>
+</body>
+
 </html>
